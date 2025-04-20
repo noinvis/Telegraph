3f4f66e883f190458dd343db6c124339ce05e55d
noinvis <lazizbek.sn@gmail.com>
2025-04-21 00:21:43 +0500

    Telegraph

---
+++ src/app.js
@@ -0,0 +1,276 @@
+{
+    function darkMode(){
+        document.documentElement.classList.toggle("dark")
+    }
+}
+{
+    // 1 - task
+    // let weekNumber = 1
+    // let week = ""
+    // switch (weekNumber) {
+    //     case 1:
+    //         week = "Dushanba";
+    //         console.log(week);
+    //         break;
+    //     case 2:
+    //         week = "Seyshanba";
+    //         console.log(week);
+    //         break;
+    //     case 3:
+    //         week = "Chorshanba";
+    //         console.log(week);
+    //         break;
+    //     case 4:
+    //         week = "Payshanba";
+    //         console.log(week);
+    //         break;
+    //     case 5:
+    //         week = "Juma";
+    //         console.log(week);
+    //         break;
+    //     case 6:
+    //         week = "Shanba";
+    //         console.log(week);
+    //         break;
+    //     case 7:
+    //         week = "Yakshanba";
+    //         console.log(week);
+    //         break;
+    //     default:
+    //         week = "Boshqa hafta kuni kiritildi"
+    //         console.log(week);
+    //         break;
+    // }
+}
+
+{
+    // 2 - task 
+    // let K = 2
+    // let result = ""
+    // switch(K){
+    //     case 1:
+    //         result = "yomon";
+    //         console.log(result);
+    //         break;
+    //     case 2:
+    //         result = "qoniqarsiz";
+    //         console.log(result);
+    //         break;
+    //     case 3:
+    //         result = "qoniqarli";
+    //         console.log(result);
+    //         break;
+    //     case 4:
+    //         result = "yahshi";
+    //         console.log(result);
+    //         break;
+    //     case 5:
+    //         result = "a'lo";
+    //         console.log(result);
+    //         break;
+    //     default:
+    //         result = "Boshqa son kiritildi";
+    //         console.log(result);
+    //         break;
+    // } 
+}
+
+{
+    // 3 - task
+    // let monthNumber = 4
+    // let month = ""
+    // switch (monthNumber) {
+    //     case 1:
+    //     case 2:
+    //     case 12:
+    //         month = "Qish";
+    //         console.log(month);
+    //         break;
+    //     case 3:
+    //     case 4:
+    //     case 5:
+    //         month = "Bahor";
+    //         console.log(month);
+    //         break;
+    //     case 6:
+    //     case 7:
+    //     case 8:
+    //         month = "Yoz";
+    //         console.log(month);
+    //         break;
+    //     case 9:
+    //     case 10:
+    //     case 11:
+    //         month = "Kuz";
+    //         console.log(month);
+    //         break;
+    //     default:
+    //         week = "Boshqa oy kiritildi"
+    //         console.log(week);
+    //         break;
+    // }
+}
+
+{
+    // 4 - task 
+    // let monthNumber = 12
+    // let month = ""
+    // switch (monthNumber) {
+    //     case 1:
+    //         month = "Yanvarda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 2:
+    //         month = "Fevralda 28 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 3:
+    //         month = "Martda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 4:
+    //         month = "Aprelda 30 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 5:
+    //         month = "Mayda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 6:
+    //         month = "Iyunda 30 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 7:
+    //         month = "Iyulda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 8:
+    //         month = "Avgustda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 9:
+    //         month = "Sentyabrda 30 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 10:
+    //         month = "Oktyabrda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 11:
+    //         month = "Noyabrdada 30 kun bor";
+    //         console.log(month);
+    //         break;
+    //     case 12:
+    //         month = "Dekabrda 31 kun bor";
+    //         console.log(month);
+    //         break;
+    //     default:
+    //         week = "Boshqa oy kiritildi"
+    //         console.log(week);
+    //         break;
+    // }
+}
+
+{
+    // 5 - task 
+    // let A = 10;
+    // let B = 2;
+    // let amal = prompt("Amalni tanlang (1-qo'shish, 2-ayirish, 3-bo'lish, 4-ko'paytirish):");
+    // let natija;
+    // switch(+amal) {
+    //     case 1:
+    //         natija = A + B;
+    //         console.log(`${A} + ${B} = ${natija}`);
+    //         break;
+    //     case 2:
+    //         natija = A - B;
+    //         console.log(`${A} - ${B} = ${natija}`);
+    //         break;
+    //     case 3:
+    //         if (B !== 0) {
+    //             natija = A / B;
+    //             console.log(`${A} / ${B} = ${natija}`);
+    //         } else {
+    //             console.log("Xato: Nolga bo'lish mumkin emas!");
+    //         }
+    //         break;
+    //     case 4:
+    //         natija = A * B;
+    //         console.log(`${A} * ${B} = ${natija}`);
+    //         break;
+    //     default:
+    //         console.log("Noto'g'ri amal tanlandi!");
+    // }
+}
+
+{
+    // 6 - task 
+    // let birlik = prompt("Uzunlik birligini tanlang:\n1 - desimetr\n2 - kilometr\n3 - metr\n4 - millimetr\n5 - santimetr");
+    // let qiymat = prompt("Uzunlik qiymatini kiriting:");
+    // let metrda;
+    // let birlikNomi;
+
+    // if (birlik === 1) {
+    //     metrda = qiymat / 10;
+    //     birlikNomi = "desimetr";
+    // } else if (birlik === 2) {
+    //     metrda = qiymat * 1000;
+    //     birlikNomi = "kilometr";
+    // } else if (birlik === 3) {
+    //     metrda = qiymat;
+    //     birlikNomi = "metr";
+    // } else if (birlik === 4) {
+    //     metrda = qiymat / 1000;
+    //     birlikNomi = "millimetr";
+    // } else if (birlik === 5) {
+    //     metrda = qiymat / 100;
+    //     birlikNomi = "santimetr";
+    // } else {
+    //     console.log("Noto'g'ri birlik tanlandi!");
+    //     birlikNomi = "noma'lum birlik";
+    //     metrda = null;
+    // }
+
+    // if (metrda !== null) {
+    //     console.log(`${qiymat} ${birlikNomi} = ${metrda} metr`);
+    // }
+}
+
+{
+    // 7 - task 
+    // let birlik = prompt("Og'irlik birligini tanlang:\n1 - kilogram\n2 - milligram\n3 - gram\n4 - tonna\n5 - sentner");
+    // let qiymat = prompt("Og'irlik qiymatini kiriting:");
+    // let kg;
+    // let birlikNomi;
+
+    // switch(+birlik) {
+    //     case 1:
+    //         kg = +qiymat;
+    //         birlikNomi = "kilogram";
+    //         break;
+    //     case 2:
+    //         kg = +qiymat / 1000000;
+    //         birlikNomi = "milligram";
+    //         break;
+    //     case 3:
+    //         kg = +qiymat / 1000;
+    //         birlikNomi = "gram";
+    //         break;
+    //     case 4:
+    //         kg = +qiymat * 1000;
+    //         birlikNomi = "tonna";
+    //         break;
+    //     case 5:
+    //         kg = +qiymat * 100;
+    //         birlikNomi = "sentner";
+    //         break;
+    //     default:
+    //         console.log("Noto'g'ri birlik tanlandi!");
+    //         birlikNomi = "noma'lum birlik";
+    //         kg = null;
+    // }
+
+    // if (kg !== null) {
+    //     console.log(`${+qiymat} ${birlikNomi} = ${kg} kilogram`);
+    // }
+}
